81030e8da7cafd457e690c6a9a35ab64cbd8c02c
kwame-Owusu <alfredowusu595@gmail.com>
2025-08-18 21:32:21 +0100

    started adding commands for the REPL

--- main.go
+++ main.go
@@ -1,26 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
 func main() {
-	for {
-
-		scanner := bufio.NewScanner(os.Stdin)
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		userInput := scanner.Text()
-		clean := cleanInput(userInput)
-		fmt.Printf("Your command was: %v\n", clean[0])
-	}
-
-}
-
-func cleanInput(text string) []string {
-	result := strings.ToLower(text)
-	return strings.Fields(result)
+	startRepl()
 }
