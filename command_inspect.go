185c58b9b4736df687a48139886290119cff5bd5
kwame-Owusu <alfredowusu595@gmail.com>
2025-08-31 21:12:07 +0100

    added command inspection functionality, it works, will constrain it to pokemons we have caught

--- command_inspect.go
+++ command_inspect.go
@@ -1,5 +1,26 @@
 package main
 
+import "fmt"
+
 func commandInspect(config *config, args ...string) error {
+	pokemon := args[0]
+	inspectionResp, err := config.pokeapiClient.Inspect(pokemon)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Name : %s\n", pokemon)
+	fmt.Printf("Base Experience: %d\n", inspectionResp.BaseExp)
+	fmt.Printf("Height: %d\n", inspectionResp.Height)
+	fmt.Printf("Weight: %d\n", inspectionResp.Weight)
+	fmt.Println("Stats: ")
+	for _, stat := range inspectionResp.Stats {
+		fmt.Printf("- %s: %d\n", stat.Stat.Name, stat.BaseStat)
+	}
+	fmt.Println("Types: ")
+	for _, t := range inspectionResp.Types {
+		fmt.Printf("- %s\n", t.Type.Name)
+	}
+
 	return nil
 }
