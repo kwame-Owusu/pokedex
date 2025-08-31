ad4a78d74dd98d75f6fbe85602b0f9a1cdcf2932
kwame-Owusu <alfredowusu595@gmail.com>
2025-08-31 20:35:39 +0100

    inspect command

---
+++ command_inspect.go
@@ -0,0 +1,5 @@
+package main
+
+func commandInspect(config *config, args ...string) error {
+	return nil
+}
---
+++ internal/pokeapi/inspect_pokemon.go
@@ -0,0 +1,5 @@
+package pokeapi
+
+func (c *Client) Inspect(pokemon string) error {
+	return nil
+}
