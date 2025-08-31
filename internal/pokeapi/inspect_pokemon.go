4a65e249c8c9c7d2505733a17b0ebda1ac4e7c31
kwame-Owusu <alfredowusu595@gmail.com>
2025-08-31 20:52:42 +0100

    wip, inspect api complete with more details on pokemonDetails

--- internal/pokeapi/inspect_pokemon.go
+++ internal/pokeapi/inspect_pokemon.go
@@ -1,5 +1,31 @@
 package pokeapi
 
-func (c *Client) Inspect(pokemon string) error {
-	return nil
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func (c *Client) Inspect(pokemon string) (PokemonDetails, error) {
+	url := baseURL + "/pokemon/" + pokemon
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return PokemonDetails{}, err
+	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return PokemonDetails{}, err
+	}
+	defer resp.Body.Close()
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return PokemonDetails{}, err
+	}
+
+	pokemonDetails := PokemonDetails{}
+	err = json.Unmarshal(data, &pokemonDetails)
+	return pokemonDetails, nil
 }
--- internal/pokeapi/types_locations.go
+++ internal/pokeapi/types_locations.go
@@ -28,6 +28,17 @@ type PokemonDetails struct {
 	BaseExp int `json:"base_experience"`
 	Height  int `json:"height"`
 	Weight  int `json:"weight"`
+	Stats   []struct {
+		BaseStat int `json:"base_stat"`
+		Stat     struct {
+			Name string `json:"name"`
+		} `json:"stat"`
+	} `json:"stats"`
+	Types []struct {
+		Type struct {
+			Name string `json:"name"`
+		} `json:"type"`
+	} `json:"types"`
 }
 
 type Pokedex struct {
